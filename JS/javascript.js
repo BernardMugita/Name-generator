35fa36e8b2baf83743d3f28223b2dda745c74dde
Bernard Mugita <mugita68@gmail.com>
2019-07-15 20:48:58 +0300

    This is an update on the javascript file

--- JS/javascript.js
+++ JS/javascript.js
@@ -8,3 +8,19 @@ function makeArray(n){
  var days=["Saturday","Sunday","Monday","Tuesday","Wednesday","Thursday","Friday"];
  
  var months=["undefined","January","February","March","April","May","June","July","August","September","October","November","December"];
+
+function calculate(form){
+   var val1=parseInt(form.day.value,10);
+   if((val1<=0)||(val1>31)){
+       alert("Invalid! Please enter the correct day")
+   }
+   var val2=parseInt(form.month.value,10);
+   if ((val2<=0)||(val2>12)){
+       alert("Invalid! Please enter the correct month")
+   }
+   var val2x=parseInt(form.month.value,10);
+   var val3=parseInt(form.year.value,10);
+   if (val3<1900){
+       alert("You can't be that old!")
+   }
+}
